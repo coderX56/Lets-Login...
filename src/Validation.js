2d47f50c332f069b48fdb016ed412baf6a33f235
Aman Tiwari <114355313+coderX56@users.noreply.github.com>
2024-06-26 00:21:57 +0530

    Update Validation.js

--- src/Validation.js
+++ src/Validation.js
@@ -1,28 +1,28 @@
 export default function Validation(formData) {
-    const errors = {};
-    
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,6}$/;
-    const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;
-     
+  const errors = {};
 
-    if(formData.name ===""){
-      errors.name="*Name is required"};
-    
-    if (formData.email === "") {
-      errors.email = "*Email is required";
-    } else if (!emailPattern.test(formData.email)) {
-      errors.email = "*Email is invalid";
-    }
-  
-    if (formData.password === "") {
-      errors.password = "*Password is required";
-    } else if (!passwordPattern.test(formData.password)) {
-      errors.password = "*Password is invalid";
-    }
-    if(formData.password!==formData.cpassword){
-      errors.cpassword="*Passwword do not match";
-    }
-  
-    return errors;
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,6}$/;
+  const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
+  if (formData.name === "") {
+    errors.name = "*Name is required";
+  }
+
+  if (formData.email === "") {
+    errors.email = "*Email is required";
+  } else if (!emailPattern.test(formData.email)) {
+    errors.email = "*Email is invalid";
+  }
+
+  if (formData.password === "") {
+    errors.password = "*Password is required";
+  } else if (!passwordPattern.test(formData.password)) {
+    errors.password = "*Password is invalid";
   }
   
+  if (formData.password !== formData.cpassword) {
+    errors.cpassword = "*Passwords do not match";
+  }
+
+  return errors;
+}
